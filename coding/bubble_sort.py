172353f07240957928d5d004a7bcfab135806f3f
Xin Xiang <xxiang@mail.yu.edu>
2024-07-28 00:22:42 -0400

    Initial commit

---
+++ coding/bubble_sort.py
@@ -0,0 +1,11 @@
+# filename: bubble_sort.py
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+arr = [4, 1, 5, 2, 3]
+print(bubble_sort(arr))
